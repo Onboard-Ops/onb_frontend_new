f1a658f8a3dfa79404c8187a6df75a4164abf669
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-09-28 16:18:36 +0530

    URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:5000/api/v1";
+export const API_URL = "http://206.189.206.202/api/v1";
 // "https://walrus-app-z3mvj.ondigitalocean.app/ops-backend-new2/api/v1";
