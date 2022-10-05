c19954a11acbc3c2a437acc06e4bbc9996480a47
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-10-05 19:29:36 +0530

    Changes

--- src/redux/actions/people/people.action.js
+++ src/redux/actions/people/people.action.js
@@ -7,6 +7,8 @@ const {
   PEOPLE_API_CALL_OFF,
   PEOPLE_API_LOADER_ON,
   PEOPLE_API_LOADER_OFF,
+  PEOPLE_API_MOADL_ON,
+  PEOPLE_API_MOADL_OFF,
   PEOPLE_API_DATA,
 } = PeopleTypes;
 
@@ -37,7 +39,7 @@ export const FetchPeopleApi = () => async (dispatch) => {
   }
 };
 
-export const CreateUserApi = (formData) => async (dispatch) => {
+export const CreateUserApi = (formData, onClose) => async (dispatch) => {
   try {
     console.log(formData, "DATA");
     dispatch({
@@ -48,7 +50,9 @@ export const CreateUserApi = (formData) => async (dispatch) => {
     });
     const res = await axios.post(`${API_URL}/signup`, formData, config);
     res?.status && dispatch({ type: PEOPLE_API_LOADER_OFF });
+    console.log(res);
     res?.status && dispatch(FetchPeopleApi());
+    onClose();
   } catch (error) {
     console.log(error);
   }
